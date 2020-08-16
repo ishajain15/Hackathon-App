91d6c518d73e277102c08f9956c3f07cf79bce31
Isha Jain <write2ishajain@gmail.com>
2020-08-16 00:05:53 -0400

    Stylized the sign in button, added image and custom font

--- App/screens/LoginScreen.js
+++ App/screens/LoginScreen.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar } from "expo-status-bar";
 import * as Google from "expo-google-app-auth";
+import * as Font from "expo-font";
+
+let customFonts = {
+  "Inter-Black": require("../assets/fonts/Inter-Black.ttf"),
+};
 
 class LoginScreen extends Component {
+  state = {
+    fontsLoaded: false,
+  };
+
   signInWithGoogleAsync = async () => {
     try {
       const result = await Google.logInAsync({
@@ -25,24 +41,45 @@ class LoginScreen extends Component {
     }
   };
 
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <LinearGradient
-          colors={["white", "rgb(149, 156, 241)"]}
-          style={styles.linearGradient}
-          start={{ x: 0.5, y: 0 }}
-          end={{ x: 0.5, y: 0.5 }}
-        >
-          <Text>hi my name is unnati</Text>
-          <Button
-            title="Sign in with Google"
-            onPress={() => this.signInWithGoogleAsync()}
-          />
-        </LinearGradient>
-        <StatusBar style="auto" />
-      </View>
-    );
+    if (this.state.fontsLoaded) {
+      return (
+        <View style={styles.container}>
+          <LinearGradient
+            colors={["white", "rgb(149, 156, 241)"]}
+            style={styles.linearGradient}
+            start={{ x: 0.5, y: 0 }}
+            end={{ x: 0.5, y: 0.5 }}
+          >
+            <Text>hi my name is unnati</Text>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => this.signInWithGoogleAsync()}
+            >
+              <Text style={styles.text}>
+                <Image
+                  style={styles.image}
+                  source={require("../assets/google.png")}
+                />
+                SIGN IN WITH GOOGLE
+              </Text>
+            </TouchableOpacity>
+          </LinearGradient>
+          <StatusBar style="auto" />
+        </View>
+      );
+    } else {
+      return <Text>Didn't Work :(</Text>;
+    }
   }
 }
 
@@ -60,5 +97,22 @@ const styles = StyleSheet.create({
     borderRadius: 0,
     height: 700,
     width: 450,
+  },
+  button: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 10,
+  },
+  image: {
+    height: 20,
+    width: 20,
+  },
+  text: {
+    textAlign: "center",
+    paddingLeft: 10,
+    fontSize: 20,
+    color: "#475BD6",
+    fontFamily: "Inter-Black",
+    lineHeight: 24,
   },
 });
