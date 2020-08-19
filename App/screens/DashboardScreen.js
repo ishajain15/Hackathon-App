e62bdf9b18f0ebcd69c7c806e73b70757a296584
RamithaKotarkonda <rkotarko@purdue.edu>
2020-08-19 01:24:22 -0500

    background commit

--- App/screens/DashboardScreen.js
+++ App/screens/DashboardScreen.js
@@ -1,22 +1,32 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 class DashboardScreen extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>DashboardScreen</Text>
-      </View>
-    );
+      //if (this.state.fontsLoaded) {
+        return (
+          <View style={styles.container}>
+            <LinearGradient
+              colors={["white", "rgb(149, 156, 241)"]}
+              style={styles.linearGradient}
+              start={{ x: 0.5, y: 0 }}
+              end={{ x: 0.5, y: 0.5 }}
+            >
+          </LinearGradient>
+          </View>
+        )}
   }
-}
 
 export default DashboardScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  linearGradient: {
     alignItems: "center",
     justifyContent: "center",
+    flex: 1,
   },
 });
