b3174702672f9a79fcf151b0afb41256dc678566
RamithaKotarkonda <rkotarko@purdue.edu>
2020-08-19 02:37:27 -0500

    picker

--- App/screens/DashboardScreen.js
+++ App/screens/DashboardScreen.js
@@ -1,22 +1,33 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-
+import { Picker } from "@react-native-community/picker";
 class DashboardScreen extends Component {
   render() {
-      //if (this.state.fontsLoaded) {
-        return (
-          <View style={styles.container}>
-            <LinearGradient
-              colors={["white", "rgb(149, 156, 241)"]}
-              style={styles.linearGradient}
-              start={{ x: 0.5, y: 0 }}
-              end={{ x: 0.5, y: 0.5 }}
-            >
-          </LinearGradient>
-          </View>
-        )}
+    //if (this.state.fontsLoaded) {
+    return (
+      <View style={styles.container}>
+        <LinearGradient
+          colors={["white", "rgb(149, 156, 241)"]}
+          style={styles.linearGradient}
+          start={{ x: 0.5, y: 0 }}
+          end={{ x: 0.5, y: 0.5 }}
+        >
+          <Picker
+            //selectedValue={this.state.language}
+            style={{ height: 50, width: 100 }}
+            onValueChange={(itemValue, itemIndex) =>
+              this.setState({ language: itemValue })
+            }
+          >
+            <Picker.Item label="Java" value="java" />
+            <Picker.Item label="JavaScript" value="js" />
+          </Picker>
+        </LinearGradient>
+      </View>
+    );
   }
+}
 
 export default DashboardScreen;
 
